6cff19c2f19562e0e8c7ecbc71d77344c6cc0312
Hồ Xuân Hùng <38959941+xuanhungho@users.noreply.github.com>
2018-12-21 13:30:59 +0700

    Add files via upload

---
+++ src/main/java/controller/ICreate.java
@@ -0,0 +1,10 @@
+package controller;
+
+public interface ICreate {
+	public String randomDinhDanh(int id);
+	public String randomMoTa();
+	public String randomThoiGian(int i);
+	public String randomLink();
+	public String randomNhan();
+	public void CreateNode(int num);
+}
